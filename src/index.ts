9add6628884d77447df4d12768acc0973ea9a99b
MRNafisiA <mnafisiasl@gmail.com>
2022-06-22 11:14:16 +0430

    add unwrap function

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,11 @@ export type Err<E> = { ok: false; error: E };
 
 export const ok = <V>(value: V): Ok<V> => ({ok: true, value});
 export const err = <E>(error: E): Err<E> => ({ok: false, error});
+
+export const unwrap = <V>(result: Result<V, any>): unknown extends V ? never : V => {
+    if (result.ok) {
+        return result.value as any;
+    } else {
+        throw result.error;
+    }
+};

46282a326a2e1497c712e893c156d6646a89efe8
MRNafisiA <mnafisiasl@gmail.com>
2023-12-27 09:14:45 +0330

    update config files.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
-export type Result<V, E> = Ok<V> | Err<E>;
-export type Ok<V> = { ok: true; value: V };
-export type Err<E> = { ok: false; error: E };
+type Result<V, E> = Ok<V> | Err<E>;
+type Ok<V> = { ok: true; value: V };
+type Err<E> = { ok: false; error: E };
 
-export const ok = <V>(value: V): Ok<V> => ({ok: true, value});
-export const err = <E>(error: E): Err<E> => ({ok: false, error});
+const ok = <V>(value: V): Ok<V> => ({ ok: true, value });
+const err = <E>(error: E): Err<E> => ({ ok: false, error });
 
-export const unwrap = <V>(result: Result<V, any>): unknown extends V ? never : V => {
-    if (result.ok) {
-        return result.value as any;
-    } else {
-        throw result.error;
-    }
-};
+export type { Result, Ok, Err };
+export { ok, err };
